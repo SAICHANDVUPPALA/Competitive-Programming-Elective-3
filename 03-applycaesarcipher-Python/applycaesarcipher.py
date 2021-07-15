97373abd39e78945c72102ea30baf6265b6679ce
SAICHANDVUPPALA <saichand9001@msitprogram.net>
2021-07-15 19:25:21 +0530

    Done

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -11,7 +11,26 @@
 
 
 def fun_applycaesarcipher(msg, shift):
-	return ""
+	n=len(msg)
+	result=""
+	if(shift!=0):
+		for i in range(n):
+			orascii=ord(msg[i])
+			if(orascii==32):
+				ascii=orascii
+			else:
+				ascii=orascii+shift
+				if(ascii>90 and ascii<97 and orascii<91):
+					ascii=ascii-90+64
+				elif(ascii<65):
+					ascii=91-(65-ascii)
+				elif(ascii>90 and ascii<97 and orascii>96):
+					ascii=123-(97-ascii)
+				elif(ascii>122):
+					ascii=ascii-122+96
+			result+=chr(ascii)
+		
+	return result
 
 
 
