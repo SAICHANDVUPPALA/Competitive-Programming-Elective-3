2656b50efe626fb240d19d7a836d49766a8c0e17
SAICHANDVUPPALA <saichand9001@msitprogram.net>
2021-07-19 20:06:31 +0530

    Done

--- removeRowAndCol.py
+++ removeRowAndCol.py
@@ -23,6 +23,17 @@
 
 def removeRowAndCol(L, row, col):
     # Your code goes here...
-    pass
-
+    if(row>len(L) and col>len(L[0])):
+        return "Not Possible"
+    elif(row<=0 and col<=0):
+        return "Not Possible"
+    else:
+        for i in L:
+            del i[col]
+        del L[row]
+        
+    return L
+print(removeRowAndCol([[2,3,4,5],[8,7,6,5],[0,1,2,3]],1,2))
+print(removeRowAndCol([[2,3,4,5],[8,7,6,5],[0,1,2,3]],0,-1))
+print(removeRowAndCol([[2,3,4,5],[8,7,6,5],[0,1,2,3]],6,10))
 # Write your own test cases.
