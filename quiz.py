f134227cfcea9dd226eeb9f4131d317cbdc430ac
SAICHANDVUPPALA <saichand9001@msitprogram.net>
2021-08-03 20:29:33 +0530

    Done

--- quiz.py
+++ quiz.py
@@ -1,12 +1,37 @@
-# Write the function bestQuiz(a), which takes a rectangular 2d list of numbers that represents a gradebook, where each column represents a quiz, and each row represents a student, and each value represents that student's score on that quiz (except -1 indicates the student did not take the quiz). For example:
+# Write the function bestQuiz(a), which takes a rectangular 2d list of numbers that represents a gradebook, where each column represents a quiz, 
+# and each row represents a student, and each value represents that student's score on that quiz (except -1 indicates the student did not take the 
+# quiz). For example:
 #   a = [ [ 88,  80, 91 ],
 #         [ 68, 100, -1 ]
 #       ]
-# This list indicates that student0 scored 88 on quiz0, 80 on quiz1, and 91 on quiz2. Also, student1 scored 68 on quiz0, 100 on quiz1, and did not take quiz2. The function returns the quiz with the highest average. In this case, quiz0 average is 78, quiz1 average is 90, and quiz2 average is 91 (since we ignore the -1). Thus, quiz2 is the best, and so the function returns 2 in this case. You are not responsible for malformed input, except you should return None if there are no quizzes. Also, resolve ties in favor of the lower quiz number. Here is a test function for you:
+# This list indicates that student0 scored 88 on quiz0, 80 on quiz1, and 91 on quiz2. Also, student1 scored 68 on quiz0, 100 on quiz1, and did not 
+# take quiz2. The function returns the quiz with the highest average. In this case, quiz0 average is 78, quiz1 average is 90, and quiz2 average 
+# is 91 (since we ignore the -1). Thus, quiz2 is the best, and so the function returns 2 in this case. You are not responsible for malformed 
+# input, except you should return None if there are no quizzes. Also, resolve ties in favor of the lower quiz number. Here is a test function 
+# for you:
 
 def bestQuiz(l):
     # Your  code goes ehre...
-    pass
+      sums=[]
+      for i in range(len(l[0])):
+            sum=0
+            count=0
+            for j in range(len(l)):
+                  if(l[j][i]!=-1):
+                        sum+=l[j][i]
+                        count+=1
+            if(count!=0):
+                  avg=sum/count
+                  sums.append(avg)
+      if(len(sums)==0):
+            return None
+      m=sums.index(max(sums))
+      return m
+
+                  
+            
+
+
 
 def testBestQuiz():
     print('Testing bestQuiz()...', end='')
@@ -23,3 +48,5 @@ def testBestQuiz():
           [-1, -1, -1 ]]
     assert(bestQuiz(a) == None)
     print('All test cases passed...!')
+
+testBestQuiz()
