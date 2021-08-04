fdf6b0cbfe6aa6b00e1113bf0286c1cf3d30b6c7
SAICHANDVUPPALA <saichand9001@msitprogram.net>
2021-08-04 21:59:36 +0530

    Done

--- LatinSquare.py
+++ LatinSquare.py
@@ -7,6 +7,36 @@
 
 # Write your own test cases...
 
-def isLatinSquare(lst):
+def isLatinSquare(a):
     # Your code goes here...
-    pass
+    N = len(a)
+    rows = []
+    for i in range(N):
+        rows.append(set([]))
+    cols = []
+    for i in range(N):
+        cols.append(set([]))
+    invalid = 0
+    for i in range(N):
+        for j in range(N):
+            rows[i].add(a[i][j])
+            cols[j].add(a[i][j])
+  
+            if (a[i][j] > N or a[i][j] <= 0) :
+                invalid += 1
+    numrows = 0
+    numcols = 0
+  
+    for i in range(N):
+        if (len(rows[i]) != N) :
+            numrows+=1
+        if (len(cols[i]) != N) :
+            numcols+=1
+    if (numcols == 0 or numrows == 0 and invalid == 0) :
+        return "Yes"
+    else:
+        return "No"
+
+
+assert(isLatinSquare([[1,2],[2,1]])=="Yes")
+print("Passed")
