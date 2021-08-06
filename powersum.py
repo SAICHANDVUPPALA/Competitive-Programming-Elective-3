a61baa1ddd6a26589c5b7b15e79eb70355ebebbf
SAICHANDVUPPALA <saichand9001@msitprogram.net>
2021-08-06 20:10:02 +0530

    Done

--- powersum.py
+++ powersum.py
@@ -12,8 +12,21 @@
 
 def powerSum(n, k):
     # Your code goes here...
-    return 0
+    if(n<0 or k<0):
+        return 0
+    sum=0
+    for i in range(1,n+1):
+        sum+=i**k
+
+    return sum
+
+def repowerSum(n,k):
+    if(k<0 or n<0):
+        return 0
+    sum=n**k
+    return sum+repowerSum(n-1,k)
 
 # Write your own test cases here...
-
+assert(repowerSum(1,3)==1)
+assert(repowerSum(5,2)==55)
 print ("All test cases passed...")
